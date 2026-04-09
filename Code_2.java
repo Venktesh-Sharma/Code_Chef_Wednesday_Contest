96217040472415ce2f3e164d06f6ed152f45faa1
Venktesh Sharma <venkteshsharma25@gmail.com>
2026-04-09 10:24:11 +0530

    Add files via upload

---
+++ Code_2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Code_2 {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 5) {
+            System.out.println(n * 85);
+        } else {
+            System.out.println(n * 100);
+        }
+    }
+}
+
